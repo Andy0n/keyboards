06a53427aaec3b77acf87cb6d175649889e01c46
Andreas Himmler <13476558+Andy0n@users.noreply.github.com>
2024-07-23 11:12:37 +0200

    refactoring: moved kmk; added submodule

--- kmk/code.py
+++ kmk/code.py
@@ -1,6 +1,6 @@
 print("Starting")
 
-from kb import KMKKeyboard 
+from kb import KMKKeyboard
 
 from kmk.keys import KC
 from kmk.modules.split import Split
@@ -16,20 +16,20 @@ keyboard.modules.append(Layers())
 
 keyboard.keymap = [
     [
-        KC.ESC,  KC.N1, KC.N2, KC.N3,   KC.N4,   KC.N5,       KC.N6,    KC.N7,  KC.N8,   KC.N9,  KC.N0,   KC.BSPC,
-        KC.TAB,  KC.Q,  KC.W,  KC.E,    KC.R,    KC.T,        KC.Y,     KC.U,   KC.I,    KC.O,   KC.P,    KC.MINS,
-        KC.LSFT, KC.A,  KC.S,  KC.D,    KC.F,    KC.G,        KC.H,     KC.J,   KC.K,    KC.L,   KC.SCLN, KC.RSFT,
-        KC.NUBS, KC.Z,  KC.X,  KC.C,    KC.V,    KC.B,        KC.N,     KC.M,   KC.COMM, KC.DOT, KC.SLSH, KC.QUOT,
-                               KC.MO(1), KC.SPC, KC.LCTRL,    KC.RCTRL, KC.ENT, KC.MO(2),
-                                        KC.RGUI, KC.BSPC,     KC.DEL,   KC.LALT    
+        KC.ESC,  KC.N1, KC.N2, KC.N3,    KC.N4,   KC.N5,       KC.N6,    KC.N7,   KC.N8,   KC.N9,  KC.N0,   KC.MINS,
+        KC.TAB,  KC.Q,  KC.W,  KC.E,     KC.R,    KC.T,        KC.Y,     KC.U,    KC.I,    KC.O,   KC.P,    KC.LBRC,
+        KC.BSLS, KC.A,  KC.S,  KC.D,     KC.F,    KC.G,        KC.H,     KC.J,    KC.K,    KC.L,   KC.SCLN, KC.QUOT,
+        KC.NUBS, KC.Z,  KC.X,  KC.C,     KC.V,    KC.B,        KC.N,     KC.M,    KC.COMM, KC.DOT, KC.SLSH, KC.RBRC,
+                               KC.MO(1), KC.SPC,  KC.BSPC,     KC.ENT,   KC.RSFT, KC.MO(2),
+                                         KC.RGUI, KC.LCTRL,    KC.RCTRL, KC.LALT
     ],
     [
         KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,
-        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.NLCK,  KC.P7,  KC.P8,  KC.P9,  KC.TRNS,  KC.TRNS,
-        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.P4,  KC.P5,  KC.P6,  KC.TRNS,  KC.TRNS,
-        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.P1,  KC.P2,  KC.P3,  KC.TRNS,  KC.TRNS,
-                                KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.TRNS,  KC.TRNS,
-                                        KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.RALT
+        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.NLCK,  KC.P7,    KC.P8,    KC.P9,    KC.TRNS,  KC.TRNS,
+        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.P4,    KC.P5,    KC.P6,    KC.TRNS,  KC.TRNS,
+        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.P1,    KC.P2,    KC.P3,    KC.TRNS,  KC.TRNS,
+                                      KC.TRNS,  KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.TRNS,  KC.TRNS,
+                                                KC.TRNS,  KC.TRNS,      KC.TRNS,  KC.RALT
     ],
     [
         KC.F12,   KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,        KC.F6,    KC.F7,    KC.F8,      KC.F9,    KC.F10,   KC.F11,
